9aafd5f14c2e11d5c3f7dcbea0734eb201938c21
Disney K. Alves Oliveira <disney_k@hotmail.com>
2026-06-14 20:12:55 -0300

    chore: initial project structure for api and web

---
+++ devbills/web/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+return(
+
+    <div>
+        DevBills
+    </div>
+)
+
+)
+}
+
+export default App
---
+++ devbills/web/src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+
+createroot(documente.getelementById('root')!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+)
