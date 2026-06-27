4b810ea4c013bc36932cc696935d9923c9ce3821
DISNEYALVESOLIVEIRA <disney_k@hotmail.com>
2026-06-27 20:25:33 -0300

    feat: complete home screen layout and components

--- interface/src/components/Button.tsx
+++ interface/src/components/Button.tsx
@@ -28,22 +28,50 @@ const Button = ({
 			"bg-primary-500 text-[#051626] font-semibold hover:bg-primary-600 active:translate-y-0",
 		outline:
 			"border border-primary-500 text-primary-500 hover:bg-primary-500/10",
-		secondary: "bg-gray-800 text-white hover:bg-gray-700",
-		success: "bg-green-500 text-[#051626] hover:brightness-90",
-		danger: "bg-red-500 text-white hover:brightness-90",
+		secondary:
+			"bg-gray-800 text-white hover:bg-gray-700",
+		success:
+			"bg-green-500 text-[#051626] hover:brightness-90",
+		danger:
+			"bg-red-500 text-white hover:brightness-90",
 	};
+
+	// Movemos o renderLoading para dentro do componente para acessar o 'children' corretamente
+	const renderLoading = () => (	
+
+		<div className="flex items-center justify-center">
+
+			<svg className="animate-spin -ml-1 mr-2 h-4 w-4 text-current" fill="none" viewBox="0 0 24 24">
+				<title>Loading Spinner</title>
+				<circle
+					className="opacity-25"
+					cx="12"
+					cy="12"
+					r="10"
+					stroke="currentColor" strokeWidth="4"></circle>
+				<path
+					className="opacity-75"
+					fill="currentColor"
+					d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+			</svg>
+
+			{children}
+
+		</div>
+		);
 
 	return (
 		<button
 			type="button"
-			className={`px-5 py-2.5 rounded-xl font-medium transition-all flex items-center justify-center gap-2
+			className={`cursor-pointerpx-5 py-2.5 rounded-xl font-medium transition-all flex items-center justify-center gap-2
         ${variantClasses[variant]}
+		${isLoading || disabled ? "opacity-50 cursor-not-allowed" : ""}
         ${className}
         ${fullWidth ? "w-full" : ""}`}
 			disabled={disabled || isLoading}
 			{...rest}
 		>
-			{children}
+			{isLoading ? renderLoading() : children}
 		</button>
 	);
 };
--- interface/src/pages/Home.tsx
+++ interface/src/pages/Home.tsx
@@ -1,11 +1,113 @@
+
+import type { JSX } from "react";
 import Button from "../components/Button";
+import { Wallet, TrendingUp, List, CreditCard, } from "lucide-react";
+
+interface Feature {
+	icon: JSX.Element;
+	title: string;
+	description: string;
+
+
+}
 
 const Home = () => {
+
+	const features: ReadonlyArray<Feature> = [
+		{
+			icon: <Wallet className="w-8 h-8 text-primary-500" />,
+			title: "Controle Financeiro",
+			description:
+				"Monitore suas despesas e receitas em um só lugar, com uma interface intuitiva e fácil de usar.",
+		},
+		{
+			icon: <TrendingUp className="w-8 h-8 text-primary-500" />,
+			title: "Relatórios",
+			description:
+				"Visualize graficamente seus gastos e entenda para onde seu dinheiro está indo.",
+		},
+		{
+			icon: <List className="w-8 h-8 text-primary-500" />,
+			title: "Categorias Personalizadas",
+			description: "Organize suas transações em categorias para melhor análise.",
+		},
+		{
+			icon: <CreditCard className="w-8 h-8 text-primary-500" />,
+			title: "Transações Ilimitadas",
+			description: "Adicione quantas transações quiser e mantenha um histórico completo de suas finanças.",
+
+		},
+	];
+
 	return (
-		<div>
-			<h2>Home</h2>
-			<Button>Clique em mim</Button>
-		</div>
+
+
+		<div className="bg-gray-950 min-h-screen">
+			<div className="container-app">
+				{/*Hero Section*/}
+				<section className="py-12 md:py-20">
+					<div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+						<div>
+
+							<h1 className="text-4xl md:text-5xl font-bold text-white  mb-6">
+								Gerencie suas finanças com o <span>DevBills</span>
+							</h1>
+							<p className="text-lg text-white  mb-8">
+								Uma plataforma simples e eficiente para controlar suas despesas e receitas. Organize suas finanças pessoais ou do sue negócio com facilidade e tenha uma visão clara de sua situação financeira.
+							</p>
+
+							<div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
+								<Button className="text-center px-6 py-3">Começar Agora</Button>
+							</div>
+						</div>
+
+					</div>
+				</section>
+
+				<section className="py-12 md:py-20 bg-gray-900 rounded-xl">
+					<div className="container-app">
+						<div className="text-center mb-12">
+							<h2 className="text-3 font-bold text-white mb-4">Recursos do DevBills</h2>
+							<p className="text-lg text-white max-w-2lx mx-auto">Nossa plataforma oferece tudo o que você precisa para manter suas finanças
+								organizadas.</p>
+						</div>
+
+						<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+							{features.map((feature,) => (
+								<div key={feature.title} className="bg-gray-800 p-6 rounded-xl *:shadow-md *:hover:shadow-lg transition-shadow duration-300">
+									<div className="mb-4 bg-primary-500/10 flex items-center justify-center w-12 h-12 rounded-full bg-gray-700">
+										{feature.icon}
+									</div>
+									<h3 className="text-xl font-sembold text-white mb-2">{feature.title}</h3>
+									<p className="text-gray-400">{feature.description}</p>
+
+								</div>
+							))}
+						</div>
+
+
+
+					</div>
+				</section>
+
+				<section className="py-12 md:py-20 bg-gray-900 rounded-xl text-center">
+					<div className="bg-gray-900 p-8  md:p12 rounded-xl shadow-md text-center border border-gray-700">
+						<h2 className="text-2xl md:text-3xl font-bold text-white mb-4" >
+							Pronto para organizar suas finanças? Comece agora mesmo com o DevBills.
+						</h2>
+						<p className="text-lg text-white text-opacity max-w-2xl mx-auto mb-8">
+							{" "}
+							Comece a usar o DevBills hoje mesmo e tenha o controle total sobre seu dinheiro. É
+							gratuito e fácil de usar!
+						</p>
+						<Button className="mx-auto mt-4 px-6 py-3">
+							Criar Conta Gratuita
+						</Button>
+					</div>
+				</section>
+
+			</div>
+		</div >
 	);
 };
 
